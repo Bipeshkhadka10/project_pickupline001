ddb85052c0337b79d9f3604bd3ed1c26c19fb95c
Bipeshkhadka <bipeshkhadka2003@gmail.com>
2024-03-03 22:28:30 +0545

    lines added

--- brain.js
+++ brain.js
@@ -1,6 +1,15 @@
 const URL='https://rizzapi.vercel.app/list';
 let btn=document.querySelector('.btns');
-let content=document.getElementsByClassName('.lines');
-
-
-
+let content=document.querySelector('.lines');
+const num = prompt("Enter the no from 1 to 20 in your list");
+ line=parseInt(num) - 1;
+ console.log(line);
+// let flitylines = 
+(async()=>{
+    let respone = await fetch(URL);
+    console.log("loading the lines....");
+    const data = await respone.json();
+    console.log(data);
+    content.innerHTML=  data[line].text;
+}
+)();
