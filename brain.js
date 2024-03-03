b6da367ac0c0d91fdbdd2a5bb10a0bf29ef5e365
Bipeshkhadka <bipeshkhadka2003@gmail.com>
2024-03-03 23:35:39 +0545

    added commit

--- brain.js
+++ brain.js
@@ -1,15 +1,23 @@
 const URL='https://rizzapi.vercel.app/list';
 let btn=document.querySelector('.btns');
 let content=document.querySelector('.lines');
-const num = prompt("Enter the no from 1 to 20 in your list");
+let next=document.querySelector('.next');
+
+ var line = 0;
+console.log(typeof(line));
+setTimeout(()=>{
+    const num = prompt("Enter the no from 1 to 20 in your list");
  line=parseInt(num) - 1;
  console.log(line);
-// let flitylines = 
-(async()=>{
+},2000)
+
+let flitylines = async()=>{
     let respone = await fetch(URL);
     console.log("loading the lines....");
     const data = await respone.json();
     console.log(data);
-    content.innerHTML=  data[line].text;
+    content.innerHTML= data[line].text;
+   
 }
-)();
+
+btn.addEventListener('click',flitylines);
